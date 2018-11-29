537526584d1b0c66e9eaf8757d497679ba5809bb
Andi <andi.sumartono@webjet.com.au>
2018-11-29 21:23:15 +1100

    add model and add first test for bubblesort service

--- Sorting.Domain.Tests/Services/BubbleSortTests.cs
+++ Sorting.Domain.Tests/Services/BubbleSortTests.cs
@@ -1,9 +1,31 @@
+using Sorting.Domain.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Sorting.Domain.Models;
+
 [TestClass]
 public class BubbleSortTests 
 {
-    [TestInitializer]
+    private BubbleSort<FullName> _bubbleSort;
+
+    [TestInitialize]
     public void Initializer()
     {
+        _bubbleSort = new BubbleSort<FullName>();
+
+    }
+
+    [TestMethod]
+    public void Sort_GivenAnEmptyArray_ShouldReturn_EmptyArray() 
+    {
+        //arrange
+        
+        //act
+        var result = _bubbleSort.Sort(new List<FullName>());
+
+        //assert
+        Assert.IsNotNull(result, "result should not be null");
+        Assert.AreEqual(result.Count, 0);
         
     }
 }
